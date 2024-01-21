40619d7985e435257de8bf5d10daa815dd8940a9
Fabricio Velasco <fabriciorvelasco@gmail.com>
2024-01-21 15:22:10 -0300

    Iniciando a implementação da classe Piece e Board

---
+++ src/boradlayer/Piece.java
@@ -0,0 +1,24 @@
+package boradlayer;
+
+public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+}
